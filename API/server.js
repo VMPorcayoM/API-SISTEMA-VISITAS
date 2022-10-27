e3d4b8f691892f49638dbebb4c5ea4fa44ae31f7
Victor Miguel Porcayo <victor.porcayo@handcloud.com.mx>
2022-10-26 21:31:35 -0500

    post y get de visita creados

--- API/server.js
+++ API/server.js
@@ -56,6 +56,45 @@ app.get('/api/type/:user', function(req, res) {
 
 });
 
+app.get('/api/visita/:folio', function(req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
+  let folio = req.params.folio || '';
+  $query = 'SELECT * from visitas where id = \'' + folio +"\'";
+    
+    conn.query($query, function(err, rows, fields) {
+      if(err){
+          console.log("An error ocurred performing the query.");
+          return;
+      }
+      return res.json(rows);
+  });
+
+});
+
+app.post('/api/visita', function(req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
+
+  //NSERT INTO `registroentrada`.`visitas` (`nombres`, `apellidoPaterno`, `apellidoMaterno`, `tipoIdentificacion`, `NoIdentificacion`, `telefono`, `correo`, `oficinaQueVisita`, `direccion`, `fechaIngreso`, `identificacionFrontal`, `identificacionTrasera`) VALUES ('Victor', 'Porcayo', 'Mercado', 'INE', '1232342341', '3138082842', 'vmkds@gmail.com', 'askdksd', 'akdmaksdk', 'kamsd', 'asdjk', 'kasjd');
+  
+  $query = `INSERT INTO visitas 
+  ('nombres','apellidoPaterno','apellidoMaterno','tipoIdentificacion',
+  'NoIdentificacion','telefono','correo','oficinaQueVisita','direccion',
+  'fechaIngreso','identificacionFrontal','identificacionTrasera')   
+   VALUES (${req.body.nombres},${req.body.apellidoPaterno}, ${req.body.apellidoMaterno},
+    ${req.body.tipoIdentificacion},${req.body.NoIdentificacion},${req.body.telefono},${req.body.correo},
+    ${req.body.oficinaQueVisita},${req.body.direccion},${req.body.fechaIngreso},${req.body.identificacionFrontal},${req.body.identificacionTrasera})`;
+        
+    conn.query($query, function(err, rows, fields) {
+      if(err){
+          console.log("An error ocurred performing the query.");
+          return;
+      }
+      return res.json(rows);
+  });
+
+});
+
+
 
 app.listen(port,()=>{
 
